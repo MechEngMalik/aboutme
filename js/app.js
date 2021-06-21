dff8c3abbcb5fd85fa65303221604c1b6cb265c7
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-21 15:54:22 +0300

    finishing

---
+++ js/app.js
@@ -0,0 +1,69 @@
+'use strict';
+let q1 = prompt('do you know me from befor?').toLowerCase;
+
+switch (q1) {
+case 'yes':
+  alert ('ok nice ;)');
+  break;
+case 'y':
+  alert('ok nice ;)');
+  break;
+default:
+  alert('sorry for you X)');
+  break;
+}
+let q2 = prompt('do you think iam an engineer?').toLowerCase;
+switch (q2) {
+case 'yes':
+  alert('ok nice ;)');
+  break;
+case 'y':
+  alert('ok nice ;)');
+  break;
+default:
+  alert('sorry for you X)');
+  break;
+}
+let q3 = prompt('am I older than 25?').toLowerCase;
+switch (q3) {
+case 'yes':
+  alert('correct answer ;)');
+  break;
+case 'y':
+  alert('correct answer ;)');
+  break;
+default:
+  alert('wrong answer X)');
+  break;
+}
+let q4 = prompt('do you want my number?').toLowerCase;
+switch (q4) {
+case 'yes':
+  alert('ok nice ;)');
+  break;
+case 'y':
+  alert('ok nice ;)');
+  break;
+default:
+  alert('sorry for you X)');
+  break;
+}
+let q5 = prompt('do you think i will be a decent programmer?').toLowerCase;
+switch (q5) {
+case 'yes':
+  alert('ok nice ;)');
+  break;
+case 'y':
+  alert('ok nice ;)');
+  break;
+default:
+  alert('sorry for you X)');
+  break;
+}
+// eslint-disable-next-line no-unused-vars
+let userName = prompt('what is your name?');
+alert('wellcome Mr/Ms ' + userName+ ' to my world' );
+alert('let\'s go ' + userName + ' to correct your info if have a wrong answer ;)');
+
+
+
