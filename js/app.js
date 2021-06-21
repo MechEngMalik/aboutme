13c249dda9e0f809a5d3756c4ef508a6590bd7ef
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-21 16:28:51 +0300

    fixing switch statment

--- js/app.js
+++ js/app.js
@@ -1,64 +1,54 @@
 'use strict';
-let q1 = prompt('do you know me from befor?').toLowerCase;
+let q1 = prompt('do you know me from befor?').toLowerCase();
 
 switch (q1) {
 case 'yes':
-  alert ('ok nice ;)');
-  break;
 case 'y':
   alert('ok nice ;)');
   break;
 default:
   alert('sorry for you X)');
-  break;
+
 }
-let q2 = prompt('do you think iam an engineer?').toLowerCase;
+let q2 = prompt('do you think iam an engineer?').toLowerCase();
 switch (q2) {
 case 'yes':
-  alert('ok nice ;)');
-  break;
 case 'y':
   alert('ok nice ;)');
   break;
 default:
   alert('sorry for you X)');
-  break;
+
 }
-let q3 = prompt('am I older than 25?').toLowerCase;
+let q3 = prompt('am I older than 25?').toLowerCase();
 switch (q3) {
 case 'yes':
-  alert('correct answer ;)');
-  break;
 case 'y':
   alert('correct answer ;)');
   break;
 default:
   alert('wrong answer X)');
-  break;
+
 }
-let q4 = prompt('do you want my number?').toLowerCase;
+let q4 = prompt('do you want my number?').toLowerCase();
 switch (q4) {
 case 'yes':
-  alert('ok nice ;)');
-  break;
 case 'y':
   alert('ok nice ;)');
   break;
 default:
   alert('sorry for you X)');
-  break;
+
 }
-let q5 = prompt('do you think i will be a decent programmer?').toLowerCase;
+let q5 = prompt('do you think i will be a decent programmer?').toLowerCase();
 switch (q5) {
 case 'yes':
-  alert('ok nice ;)');
-  break;
 case 'y':
   alert('ok nice ;)');
   break;
 default:
   alert('sorry for you X)');
-  break;
+
 }
 // eslint-disable-next-line no-unused-vars
 let userName = prompt('what is your name?');
