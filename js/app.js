51bf2aa1800e4b30b24f67dc6fa9783b81790a1f
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-22 18:30:53 +0300

    f

--- js/app.js
+++ js/app.js
@@ -1,59 +1,190 @@
 'use strict';
-let q1 = prompt('do you know me from befor?').toLowerCase();
+// let q1 = prompt('do you know me from befor?').toLowerCase();
 
-switch (q1) {
-case 'yes':
-case 'y':
-  alert('ok nice ;)');
-  break;
-default:
-  alert('sorry for you X)');
+// switch (q1) {
+// case 'yes':
+// case 'y':
+//   alert('ok nice ;)');
+//   break;
+// default:
+//   alert('sorry for you X)');
 
+// }
+// let q2 = prompt('do you think iam an engineer?').toLowerCase();
+// switch (q2) {
+// case 'yes':
+// case 'y':
+//   alert('ok nice ;)');
+//   break;
+// default:
+//   alert('sorry for you X)');
+
+// }
+// let q3 = prompt('am I older than 25?').toLowerCase();
+// switch (q3) {
+// case 'yes':
+// case 'y':
+//   alert('correct answer ;)');
+//   break;
+// default:
+//   alert('wrong answer X)');
+
+// }
+// let q4 = prompt('do you want my number?').toLowerCase();
+// switch (q4) {
+// case 'yes':
+// case 'y':
+//   alert('ok nice ;)');
+//   break;
+// default:
+//   alert('sorry for you X)');
+
+// }
+// let q5 = prompt('do you think i will be a decent programmer?').toLowerCase();
+// switch (q5) {
+// case 'yes':
+// case 'y':
+//   alert('ok nice ;)');
+//   break;
+// default:
+//   alert('sorry for you X)');
+
+// }
+// // eslint-disable-next-line no-unused-vars
+// let userName = prompt('what is your name?');
+// alert('wellcome Mr/Ms ' + userName+ ' to my world' );
+// alert('let\'s go ' + userName + ' to correct your info if have a wrong answer ;)');
+
+
+// let q6 =Number (prompt('how much is my experience?'));
+// for (let i = 0; i < 3 ; i++) {
+
+
+//   if (q6===2) {
+//     alert('correct');
+//     break;
+
+//   } else if (q6>2) {
+//     alert('too higth');
+//     q6= Number( prompt('how much is my experience?'));
+
+//   }
+//   else {
+//     alert(' too low');
+//     q6=Number( prompt('how much is my experience?'));
+
+//   }
+
+// }
+
+// alert('the answer is 2');
+
+
+// let q7 =Number( prompt('how much old am I?'));
+// for (let i = 0; i < 3 ; i++) {
+
+
+//   if (q7===28) {
+//     alert('correct');
+//     break;
+
+//   } else if (q7>28) {
+//     alert('too higth');
+//     q7 =Number( prompt('how much old am I?'));
+
+//   } else {
+//     alert(' too low');
+//     q7 =Number( prompt('how much old am I?'));
+
+//   }
+// }
+
+
+// let q8 =Number( prompt('in what year i graduated?'));
+// for (let i = 0; i < 3 ; i++) {
+
+
+//   if (q8===2017) {
+//     alert('correct');
+//     break;
+
+//   } else if (q8>2017) {
+//     alert('too higth');
+//     q8 =Number( prompt('in what year i graduated?'));
+
+//   } else {
+//     alert(' too low');
+//     q8 =Number( prompt('in what year i graduated?'));
+
+//   }
+// }
+
+// let q9 =Number( prompt('when was i born?'));
+// for (let i = 0; i < 3 ; i++) {
+
+
+//   if (q9===1993) {
+//     alert('correct');
+//     break;
+
+//   } else if (q9>1993) {
+//     alert('too higth');
+//     q9 =Number( prompt('when was i born?'));
+
+//   } else {
+//     alert(' too low');
+//     q9 =Number( prompt('when was i born?'));
+
+//   }
+// }
+
+// let q10 =Number( prompt('what is my favorite number?'));
+// for (let i = 0; i < 3 ; i++) {
+
+
+//   if (q10===7) {
+//     alert('correct');
+//     break;
+
+//   } else if (q10>7) {
+//     alert('too higth');
+//     q10 =Number( prompt('what is my favorite number?'));
+
+//   } else {
+//     alert(' too low');
+//     q10 =Number( prompt('what is my favorite number?'));
+
+//   }
+// }
+
+// let q11 =Number( prompt('how much time i traveled?'));
+// for (let i = 0; i < 3 ; i++) {
+
+
+//   if (q11===1) {
+//     alert('correct');
+//     break;
+
+//   } else if (q11>1) {
+//     alert('too higth');
+//     q11 =Number( prompt('how much time i traveled?'));
+
+//   } else {
+//     alert(' too low');
+//     q11 =Number( prompt('how much time i traveled?'));
+
+//   }
+// }
+let w1 = prompt('what is my favorite type of car?').toLowerCase;
+let arraycar = ['bmw' , 'marcedes' , 'toyota'];
+
+for (let i = 0; i <= arraycar.length; i++) {
+
+  if (w1===arraycar) {
+    alert('gg');
+    break;
+
+  } else {prompt('what is my favorite type of car?').toLowerCase;
+
+  }
 }
-let q2 = prompt('do you think iam an engineer?').toLowerCase();
-switch (q2) {
-case 'yes':
-case 'y':
-  alert('ok nice ;)');
-  break;
-default:
-  alert('sorry for you X)');
-
-}
-let q3 = prompt('am I older than 25?').toLowerCase();
-switch (q3) {
-case 'yes':
-case 'y':
-  alert('correct answer ;)');
-  break;
-default:
-  alert('wrong answer X)');
-
-}
-let q4 = prompt('do you want my number?').toLowerCase();
-switch (q4) {
-case 'yes':
-case 'y':
-  alert('ok nice ;)');
-  break;
-default:
-  alert('sorry for you X)');
-
-}
-let q5 = prompt('do you think i will be a decent programmer?').toLowerCase();
-switch (q5) {
-case 'yes':
-case 'y':
-  alert('ok nice ;)');
-  break;
-default:
-  alert('sorry for you X)');
-
-}
-// eslint-disable-next-line no-unused-vars
-let userName = prompt('what is your name?');
-alert('wellcome Mr/Ms ' + userName+ ' to my world' );
-alert('let\'s go ' + userName + ' to correct your info if have a wrong answer ;)');
-
-
-
